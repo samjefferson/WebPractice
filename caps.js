49b90f4cced16c3b2fd7034147dc2491c9995e14
samjefferson <sam_jefferson@hotmail.co.uk>
2014-10-29 00:57:57 +0000

    yeyeyeye
    fsfs

--- caps.js
+++ caps.js
@@ -12,8 +12,6 @@ var capitals =
 ["the United States of America", "Washington D.C.",0],
 ["Italy", "Rome", 0],
 ["Japan", "Tokyo", 0],
-["Greece", "Athens", 0]
-["Russia", "Moscow", 0],
 ["Poland", "Warsaw", 1],
 ["Austria", "Vienna", 1],
 ["Sweden", "Stockholm", 1],
@@ -36,13 +34,13 @@ var capitals =
 ["Pakistan", "Islamabad", 2],
 ["Morocco", "Marrakech", 2],
 ["Iceland", "Reykjavik", 2],
-["Turkey", "Istanbul", 2],
+["Turkey", "Ankara", 2],
 ["Indonesia", "Jakarta", 3],
 ["Colombia", "Bogota", 3],
 ["Peru", "Lima", 3],
 ["Venezuela", "Caracas", 3],
 ["Somalia", "Mogadishu", 3],
-["Haiti", "Port-au-Prince", 3],
+["Haiti", "Port au Prince", 3],
 ["Iran", "Tehran", 3],
 ["Chile", "Santiago", 3],
 ["Bolivia", "Le Paz", 4],
@@ -63,40 +61,46 @@ var capitals =
 
 var level = 0;
 var levelCorrect = 0;
-var l1Unasked;
-var l2Unasked;
-var l3Unasked;
-var l4Unasked;
-var l5Unasked;
 var usedNumbers = [];
 var level = 0;
-
+var currentAnswers = [];
+var buttonArray = [];
+var questionNumber;
+var correct = 0;
+var attempts = 0;
 //allows games to increase level when a certain number of correct answers are reached or there are no more wuestions in that level
 
 function levelUp()
 	{
-		if((level==0)&&((levelCorrect== 3)||(l1Unasked==0)))
+		if((level==0)&&(levelCorrect== 3))
 			{ level++;
+			levelCorrect = 0;	
 		}
-		if((level==1)&&((levelCorrect== 4)||(l2Unasked==0)))
+		if((level==1)&&(levelCorrect== 4))
 			{ level++;
+				levelCorrect = 0;
 		}
-		if ((level==2)&&((levelCorrect== 4)||(l3Unasked==0)))
-			{ level++;
+		if ((level==2)&&(levelCorrect== 4)){
+			level++;
+			levelCorrect = 0;
 		}
-		if((level==3)&&((levelCorrect== 3)||(l4Unasked==0)))
+		
+		if((level==3)&&(levelCorrect== 4))
 			{ level++;
+				levelCorrect = 0;
 		}
-		if ((level==4)&&((levelCorrect== 3)||(l5Unasked==0)))
+		if ((level==4)&&(levelCorrect== 3))
 			{ level++;
+				levelCorrect = 0
 		}
 
 }
 
 function questionSelect(levelNumber){
 
-	var currentAnswersTwo = answersSelect(levelNumber);
-	var questionNumber; 
+
+
+	var currentAnswersTwo=currentAnswers;
 
 	for(var i=0; i<currentAnswersTwo.length; i++){
 		if(capitals[currentAnswersTwo[i]][2]== levelNumber){
@@ -104,27 +108,31 @@ function questionSelect(levelNumber){
 		}
 	}
 
-	alert(questionNumber);
-	return questionNumber;
 
 }
 
 
+
 function answersSelect(levelNumber){
 
-	var currentAnswers= [];
 	var uniqueAnswer;
 	var newAnswerInt;
 	var levelCorrect = false;
 	var counter = 0;
 
 	while(levelCorrect == false){
-
 		currentAnswers = [];
+		
 	
 		while (currentAnswers.length < 4){
 			uniqueAnswer = true;
-			newAnswerInt = Math.floor(Math.random() * 56);
+			newAnswerInt = Math.floor(Math.random() * 54);
+			//loop through unsednumber array to check if it has already been answered correctly
+			for(var u=0; u<usedNumbers.length; u++){
+				if(newAnswerInt== usedNumbers[u]){
+					uniqueAnswer = false;
+				}
+			}	
 			//Loop through array of selected songs to check unique.
 			for(var j=0; j<currentAnswers.length; j++){
 				if (newAnswerInt == currentAnswers[j]){
@@ -148,37 +156,172 @@ function answersSelect(levelNumber){
 		}
 	}
 	
-	return currentAnswers;
 }
 
 function fillButtons(){
+	
 
-	var currentAnswersThree = answersSelect(level);
-	var usedAnswer == false;
-	var buttonArray = [];
+	var currentAnswersThree = currentAnswers;
+
+	var uniqueButton;
+	buttonArray = [];
+	var butRand;
 
 	while(buttonArray.length<4){
-		var butRand = Math.floor(Math.random()*3);
-		for(i=0; i<buttonArray.length; i++){
-			if(butRand==buttonArray[i]){
-				usedAnswer=true;
+		uniqueButton = true;
+		butRand = Math.floor(Math.random()*4);
+		for(var p=0; p<buttonArray.length; p++){
+			if(butRand==buttonArray[p]){
+				uniqueButton=false;
 			}
 		}
 
-		if(usedAnswer==false){
+		if(uniqueButton == true){
 			buttonArray.push(butRand);
 		}
 		
 
 	}
+	
 
-	$buttonOne.value = (capitals[currentAnswersThree[buttonArray[0]]][1]);
-	$buttonTwo.value = (capitals[currentAnswersThree[buttonArray[1]]][1]);
-	$buttonThree.value = (capitals[currentAnswersThree[buttonArray[2]]][1]);
-	$buttonFour.value = (capitals[currentAnswersThree[buttonArray[3]]][1]);
+	document.getElementById("buttonOne").value = (capitals[currentAnswersThree[buttonArray[0]]][1]);
+	document.getElementById("buttonTwo").value = (capitals[currentAnswersThree[buttonArray[1]]][1]);
+	document.getElementById("buttonThree").value = (capitals[currentAnswersThree[buttonArray[2]]][1]);
+	document.getElementById("buttonFour").value = (capitals[currentAnswersThree[buttonArray[3]]][1]);
 
-	return buttonArray;
 }
+
+function fillQuestion(levelNumber){
+
+
+
+	var questi = document.getElementById("capital");
+
+	questi.innerHTML = (capitals[questionNumber][0]);
+
+	
+
+
+}
+
+function scoreCounter(){
+	document.getElementById("correct").innerHTML = correct;
+	document.getElementById("attempts").innerHTML = attempts;
+}
+
+function loadGame(){
+	
+	answersSelect(level);
+	questionSelect(level);
+	fillQuestion(level);
+	fillButtons();
+	
+	scoreCounter();
+	
+}
+
+function checkAnswer(){
+	
+	attempts++;
+	
+	levelUp();
+	var counterCheck;
+	
+	for(var q=0; q<buttonArray.length; q++){
+		if(currentAnswers[buttonArray[q]]==questionNumber){
+			counterCheck= q;
+			
+		}
+	}
+	
+	return counterCheck;
+}
+
+function clickButOne(){
+	var numberOne = checkAnswer();
+	if(numberOne==0){
+		document.getElementById("result").innerHTML =("CORRECT");
+			correct++;
+			levelCorrect++;
+			usedNumbers.push(questionNumber);
+	}
+	if(numberOne!=0){
+		document.getElementById("result").innerHTML = ("INCORRECT!");
+	}
+	if(attempts<20){
+		loadGame();
+	}
+	if(attempts==20){
+		document.getElementById("attempts").innerHTML= (attempts);
+		document.getElementById("correct").innerHTML= (correct);
+		document.getElementById("endMessage").innerHTML= ("Congratulations you have completed the quiz.  You have scored "+correct+" out of 20!");
+	}
+
+}
+
+function clickButTwo(){
+	var numberTwo = checkAnswer();
+	if(numberTwo==1){
+		document.getElementById("result").innerHTML =("CORRECT");
+			correct++;
+			levelCorrect++;
+			usedNumbers.push(questionNumber);
+	}
+	if(numberTwo!=1){
+		document.getElementById("result").innerHTML = ("INCORRECT!");
+	}
+
+	if(attempts<20){
+		loadGame();
+	}
+	if(attempts==20){
+		document.getElementById("attempts").innerHTML= (attempts);
+		document.getElementById("correct").innerHTML= (correct);
+		document.getElementById("endMessage").innerHTML= ("Congratulations you have completed the quiz.  You have scored "+correct+" out of 20!");
+	}
+}
+function clickButThree(){
+	var numberThree = checkAnswer();
+	if(numberThree==2){
+		document.getElementById("result").innerHTML =("CORRECT");
+			correct++;
+			levelCorrect++;
+			usedNumbers.push(questionNumber);
+	}
+	if(numberThree!=2){
+		document.getElementById("result").innerHTML = ("INCORRECT!");
+	}
+	if(attempts<20){
+		loadGame();
+	}
+	if(attempts==20){
+		document.getElementById("attempts").innerHTML= (attempts);
+		document.getElementById("correct").innerHTML= (correct);
+		document.getElementById("endMessage").innerHTML= ("Congratulations you have completed the quiz.  You have scored "+correct+" out of 20!");
+	}
+}
+
+function clickButFour(){
+	var numberFour = checkAnswer();
+	if(numberFour==3){
+		document.getElementById("result").innerHTML =("CORRECT");
+			correct++;
+			levelCorrect++;
+			usedNumbers.push(questionNumber);
+	}
+	if(numberFour!=3){
+		document.getElementById("result").innerHTML = ("INCORRECT!");
+	}
+	if(attempts<20){
+		loadGame();
+	}
+	if(attempts==20){
+		document.getElementById("attempts").innerHTML= (attempts);
+		document.getElementById("correct").innerHTML= (correct);
+		document.getElementById("endMessage").innerHTML= ("Congratulations you have completed the quiz.  You have scored "+correct+" out of 20!");
+	}
+}
+
 
 
 
